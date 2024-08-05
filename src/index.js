f0c3f25e81ec3e4124768b1f87d148fad54c206e
Anurag <tonythestark7@gmail.com>
2024-08-05 12:09:22 +0530

    + routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Top10 from './pages/Top 10';
+import Trending from './pages/Trending';
+import WatchList from './pages/Watchlist';
 
+const Applayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  )
+}
+
+
+const Approuter = createBrowserRouter([
+  {
+    element: <Applayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/top10", element: <Top10 /> },
+      { path: "/trending", element: <Trending /> },
+      {path: "/watchlist", element: <WatchList/>}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<RouterProvider router={Approuter} />)
