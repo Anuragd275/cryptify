0096e3ac1504b19bcc5e24faaf58ee286c95d2c9
Anurag <tonythestark7@gmail.com>
2024-08-05 12:10:04 +0530

    some design changes in the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
     return (
         <div className="border shadow-2xl">
             <nav className="flex justify-between px-4 items-center">
-                <h1 className="text-2xl font-bold py-2 cursor-pointer">Criptfy</h1>
+                <Link to={"/"}>
+                    <div className="flex">
+                        <h1 className="text-2xl font-bold cursor-pointer">Cripti</h1>
+                        <h1 className="tetx-2xl font-bold text-blue-500">fy</h1>
+                    </div>
+                </Link>
                 <navItem className="flex gap-4 text-xs">
+                    <Link to={"/top10"}>
                         <p className="cursor-pointer hover:text-zinc-500">Top 10</p>
+                    </Link>
+                    <Link to={"/trending"}>
                         <p className="cursor-pointer hover:text-zinc-500">Trending</p>
+                    </Link>
+                    <Link to={"/watchlist"}>
                         <p className="cursor-pointer hover:text-zinc-500">Watch List</p>
+                    </Link>
                 </navItem>
             </nav>
         </div>
@@ -14,4 +27,4 @@ function Navbar() {
 };
 
 
-module.exports = Navbar;
+export default Navbar;
