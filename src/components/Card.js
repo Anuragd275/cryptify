e2eea038c6ff0ff98c624fcb87f46525e2fd3b0f
Anurag <tonythestark7@gmail.com>
2024-08-08 10:47:58 +0530

    falana dhamaka

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="flex flex-col w-1/4 border mx-2 my-4 rounded-sm shadow-xl p-4">
+            <div className="flex">
+                <img className="w-[40px]" src=""></img>
+                
+            </div>
+        </div> 
+    )
+}
