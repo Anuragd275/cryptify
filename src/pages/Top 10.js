52c38b697cbd942f4c09aac295866073fa1e1d94
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2024-08-12 00:12:09 +0530

    Update Top 10.js

--- src/pages/Top 10.js
+++ src/pages/Top 10.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cards from "../components/Cards";
 
-function Top10() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-5xl font-bold">Top 10</h1>
-        </div>
-    )
-}
+const Top10 = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.coingecko.com/api/v3/search/trending/?precision=3";
+
+    fetch(url).then((response) =>
+      response.json().then((data) => setData(data))
+    );
+  }, []);
+
+  return data.length === 0 ? (
+    // <h1>Loading...</h1>
+  
+  <div class="mt-48 flex justify-center">
+    <div role="status" class="max-w-sm animate-pulse">
+      <div class="mb-4 h-3 w-64 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <div class="mb-2.5 h-2 w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+
+      <div class="mb-2.5 h-4 w-[120px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+
+
+
+  ) : (
+    <div>
+      <Cards data={data.coins} checker={"top10"} />
+    </div>
+  );
+};
 
 export default Top10;
