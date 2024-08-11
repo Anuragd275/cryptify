d346cc97736c1fa7307dc823fb63e170869e7044
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2024-08-12 00:11:46 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,27 @@
-import React from 'react';
+const Home = function () {
 
-function Home() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-5xl font-bold">Hello World</h1>
-        </div>
-    );
+  return (
+  <div class="flex flex-col justify-center items-center">
+    <div>
+      <h1 class="text-4xl font-bold pt-4 my-4">
+        Boost your productivity.<br/>
+        Start using our app today.
+      </h1>
+    </div>
+    <div>
+      <p class="text-xl m-4">
+        Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.
+      </p>
+    </div>
+    <div>
+      <div class="flex gap-4 items-center">
+      <button class="bg-indigo-600 p-2 rounded-md text-white font-semibold">Get Started</button>
+      <a href="#" class="font-semibold">Learn More →</a>
+    </div>
+    </div>
+  </div>
+
+  );
 };
 
-export default Home;
+export default Home; // Exporting the component for use in other files.
